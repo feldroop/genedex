9d80148ba9b04de8d331b6fb5baed8c4c6f23193
Felix Leander Droop <info@felix-droop.de>
2025-09-15 13:56:24 +0200

    docs: add example

---
+++ examples/basic_usage.rs
@@ -0,0 +1,20 @@
+use genedex::{FmIndexConfig, alphabet};
+
+fn main() {
+    // This example shows how to use the FM-Index in the most basic way.
+
+    let dna_n_alphabet = alphabet::ascii_dna_with_n();
+    let texts = [b"ACGT".as_slice(), b"acgtn"];
+
+    let index = FmIndexConfig::<i32>::new().construct(texts, dna_n_alphabet);
+
+    let query = b"GT";
+    assert_eq!(index.count(query), 2);
+
+    for hit in index.locate(query) {
+        println!(
+            "Found query in text {} at position {}.",
+            hit.text_id, hit.position
+        );
+    }
+}
